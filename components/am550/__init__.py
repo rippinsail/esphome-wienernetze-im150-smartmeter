83cb56c48bc7733cb818d78514b657a1bc9a4864
rippinsail <johnny.garage@gmx.at>
2024-09-01 02:23:16 +0200

    added basic am550

---
+++ components/am550/__init__.py
@@ -0,0 +1,48 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import *
+from esphome.components import uart
+
+DEPENDENCIES = ["uart"]
+CODEOWNERS = ["@bernikr"]
+AUTO_LOAD = ["text_sensor", "sensor"]
+MULTI_CONF = True
+
+CONF_AM550_ID = "am550_id"
+CONG_AM550_KEY = "key"
+
+pipsolar_ns = cg.esphome_ns.namespace("am550")
+am550Component = pipsolar_ns.class_("AM550", cg.Component)
+
+AM550_COMPONENT_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_AM550_ID): cv.use_id(am550Component),
+    }
+)
+
+def validate_key(value):
+    value = cv.string(value).replace(" ", "").upper()
+    if len(value) != 32 or any(c not in "0123456789ABCDEF" for c in value):
+        raise cv.Invalid(
+            "Key must be 16 bytes in Hex"
+        )
+    value = [int(value[i*2:i*2+2], 16) for i in range(16)]
+    return value
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(am550Component),
+        cv.Required(CONG_AM550_KEY): validate_key,
+        cv.GenerateID(CONF_RAW_DATA_ID): cv.declare_id(cg.uint8),
+        })
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    arr = cg.progmem_array(config[CONF_RAW_DATA_ID], config[CONG_AM550_KEY])
+    cg.add(var.set_key(arr))
+    yield cg.register_component(var, config)
+    yield uart.register_uart_device(var, config)
+    cg.add_library("frankboesing/FastCRC", "1.41")
+    cg.add_library("rweather/Crypto", "0.4.0")
---
+++ components/am550/am550.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "esphome/components/uart/uart.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/core/component.h"
+
+#include <FastCRC.h>
+#include <Crypto.h>
+#include <AES.h>
+#include <CTR.h>
+
+#define AM550_SENSOR(name) \
+protected: sensor::Sensor *name{}; \
+public: void set_##name(sensor::Sensor *name){this->name = name;}
+
+#define AM550_TEXT_SENSOR(name) \
+protected: text_sensor::TextSensor *name{}; \
+public: void set_##name(text_sensor::TextSensor *name){this->name = name;}
+
+
+namespace esphome {
+    namespace am550 {
+        static const char* AM550_VERSION = "0.1.2";
+        static const char* TAG = "am550";
+
+        class AM550 : public Component, public uart::UARTDevice {
+            AM550_SENSOR(active_energy_pos)
+            AM550_SENSOR(active_energy_neg)
+            AM550_SENSOR(reactive_energy_pos)
+            AM550_SENSOR(reactive_energy_neg)
+            AM550_SENSOR(active_power_pos)
+            AM550_SENSOR(active_power_neg)
+            AM550_SENSOR(reactive_power_pos)
+            AM550_SENSOR(reactive_power_neg)
+
+            AM550_TEXT_SENSOR(active_energy_pos_raw)
+            AM550_TEXT_SENSOR(active_energy_neg_raw)
+            AM550_TEXT_SENSOR(reactive_energy_pos_raw)
+            AM550_TEXT_SENSOR(reactive_energy_neg_raw)
+
+            public:
+                void dump_config() override;
+                void loop() override;
+
+                void set_key(const uint8_t *key) {this->key = key;}
+
+            private:
+                std::vector<uint8_t> receiveBuffer; // Stores the packet currently being received
+                unsigned long lastRead = 0; // Timestamp when data was last read
+                int readTimeout = 100; // Time to wait after last byte before considering data complete
+
+                FastCRC16 CRC16;
+                CTR<AES128> ctraes128;
+
+                const uint8_t *key; // Stores the decryption key
+
+                int bytes_to_int(uint8_t bytes[], int left, int right);
+                void handle_message(std::vector<uint8_t> msg);
+        };
+    }
+}
