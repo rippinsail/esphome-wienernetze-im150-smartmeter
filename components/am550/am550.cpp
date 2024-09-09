d7d6c0f19d5124a014c395fc338428635da61c1e
rippinsail <johnny.garage@gmx.at>
2024-09-09 14:56:46 +0200

    add key debug message

--- components/am550/am550.cpp
+++ components/am550/am550.cpp
@@ -68,6 +68,7 @@ namespace esphome {
             }
             
             // Decrypt
+            ESP_LOGV(TAG, "start decrypting using key: %s", key);
             uint8_t msglen = datalen - 33;
             uint8_t message[msglen] = {0};
             memcpy(message, msg.data() + 30, msglen);
