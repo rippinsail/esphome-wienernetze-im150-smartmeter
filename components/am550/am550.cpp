44af773ee76c6aa79329412ac84ef818ec9dcde1
rippinsail <johnny.garage@gmx.at>
2024-09-09 22:23:37 +0200

    debug

--- components/am550/am550.cpp
+++ components/am550/am550.cpp
@@ -32,11 +32,11 @@ namespace esphome {
         // Function to convert hexadecimal string to byte array
         std::vector<uint8_t> hexToBytes(const std::string &hex) {
             std::vector<uint8_t> bytes;
-            for (size_t i = 0; i < hex.length(); i += 2) {
+            /*for (size_t i = 0; i < hex.length(); i += 2) {
                 std::string byteString = hex.substr(i, 2);
                 uint8_t byte = static_cast<uint8_t>(std::stoi(byteString, nullptr, 16));
                 bytes.push_back(byte);
-            }
+            }*/
             return bytes;
         }
 
