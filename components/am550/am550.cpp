19c510e83346be959e59d8fe421677ca798bd255
rippinsail <johnny.garage@gmx.at>
2024-09-09 22:20:34 +0200

    debug

--- components/am550/am550.cpp
+++ components/am550/am550.cpp
@@ -32,9 +32,6 @@ namespace esphome {
         // Function to convert hexadecimal string to byte array
         std::vector<uint8_t> hexToBytes(const std::string &hex) {
             std::vector<uint8_t> bytes;
-            if (hex.length() % 2 != 0) {
-                throw std::invalid_argument("Hex string has an invalid length.");
-            }
             for (size_t i = 0; i < hex.length(); i += 2) {
                 std::string byteString = hex.substr(i, 2);
                 uint8_t byte = static_cast<uint8_t>(std::stoi(byteString, nullptr, 16));
