9850a652f188f890130e9803943beabf9531ec0d
rippinsail <johnny.garage@gmx.at>
2024-09-09 15:08:32 +0200

    log

--- components/am550/am550.cpp
+++ components/am550/am550.cpp
@@ -68,7 +68,7 @@ namespace esphome {
             }
             
             // Decrypt
-            ESP_LOGV(TAG, "start decrypting using key: %02x", key);
+            ESP_LOGV(TAG, "start decrypting using key no this: %02x", key);
             uint8_t msglen = datalen - 33;
             uint8_t message[msglen] = {0};
             memcpy(message, msg.data() + 30, msglen);
